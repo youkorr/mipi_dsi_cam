910a2cf93f041fda7422d00ebad59f0e1ba8eabc
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-12 07:08:44 +0200

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -12,29 +12,31 @@ class LVGLCameraDisplay : public Component {
   void setup() override;
   void loop() override;
   void dump_config() override;
-
+  
   void set_camera(mipi_dsi_cam::MipiDsiCam *camera) { this->camera_ = camera; }
-  void set_canvas_id(const std::string &canvas_id) { this->canvas_id_ = canvas_id; }
   void set_update_interval(uint32_t interval_ms) { this->update_interval_ = interval_ms; }
-
+  
+  // Configuration du canvas - à appeler depuis YAML on_boot
   void configure_canvas(lv_obj_t *canvas);
-
+  
+  // Vérifier si le canvas est configuré
+  bool is_canvas_configured() const { return this->canvas_obj_ != nullptr; }
+  
   float get_setup_priority() const override { return setup_priority::LATE; }
 
  protected:
-  mipi_dsi_cam::MipiDsiCam *camera_{nullptr};
+  tab5_camera::Tab5Camera *camera_{nullptr};
   lv_obj_t *canvas_obj_{nullptr};
-  std::string canvas_id_{};
-
-  uint32_t update_interval_{33};
+  
+  uint32_t update_interval_{20};  // 50 FPS par défaut
   uint32_t last_update_{0};
-
+  
+  // Statistiques
   uint32_t frame_count_{0};
+  uint32_t dropped_frames_{0};
   bool first_update_{true};
   bool canvas_warning_shown_{false};
-
-  uint32_t last_fps_time_{0};
-
+  
   void update_canvas_();
 };
 
