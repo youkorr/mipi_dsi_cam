9fa1fef2e9033c42f92922426ef0b0b0c5dec346
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-13 01:21:01 +0200

    Update sensor_mipi_csi_ov5647_vga.py

--- components/mipi_dsi_cam/sensor_mipi_csi_ov5647_vga.py
+++ components/mipi_dsi_cam/sensor_mipi_csi_ov5647_vga.py
@@ -340,7 +340,7 @@ public:
             value
         }};
         
-        auto err = i2c_->write(data, 3);
+        auto err = i2c_->write_read(data, 3, nullptr, 0); // new write
         if (err != esphome::i2c::ERROR_OK) {{
             ESP_LOGE(TAG, "I2C write failed for reg 0x%04X", reg);
             return ESP_FAIL;
@@ -354,16 +354,11 @@ public:
             static_cast<uint8_t>(reg & 0xFF)
         }};
         
-        // IMPORTANT: Write sans stop, puis read avec stop (repeated start)
-        auto err = i2c_->write(addr, 2, false);  // false = pas de STOP
+        auto err = i2c_->write_read(addr, 2, value, 1); // combine as it is meant to be used since latest updates,
+                                                       // without i2c deprecation warning.
+                                                       // does the same (write without stop, then read with stop
         if (err != esphome::i2c::ERROR_OK) {{
-            ESP_LOGE(TAG, "I2C write address failed for reg 0x%04X", reg);
-            return ESP_FAIL;
-        }}
-        
-        err = i2c_->read(value, 1);  // avec STOP automatique
-        if (err != esphome::i2c::ERROR_OK) {{
-            ESP_LOGE(TAG, "I2C read failed for reg 0x%04X", reg);
+            ESP_LOGE(TAG, "I2C cmd failed for reg 0x%04X", reg);
             return ESP_FAIL;
         }}
         
